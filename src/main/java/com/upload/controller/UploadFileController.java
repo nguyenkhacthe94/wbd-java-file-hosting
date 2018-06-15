651d649d5e9d72a1493efa830e1a16f6b1232c6c
Himedere <nguyenkhacthe94@gmail.com>
2018-06-15 14:12:42 +0700

    add edit page

--- src/main/java/com/upload/controller/UploadFileController.java
+++ src/main/java/com/upload/controller/UploadFileController.java
@@ -7,10 +7,7 @@ import com.upload.utils.StorageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
@@ -42,7 +39,6 @@ public class UploadFileController {
             uploadFile.setDescription(uploadFileForm.getDescription());
             uploadFile.setPublic(uploadFileForm.isPublic());
             uploadFile.setUploadedFile(randomName);
-
             uploadFileService.save(uploadFile);
         } catch (IOException e) {
             e.printStackTrace();
@@ -58,6 +54,50 @@ public class UploadFileController {
         Iterable<UploadFile> uploadFiles = uploadFileService.findAll();
         ModelAndView modelAndView = new ModelAndView("/index");
         modelAndView.addObject("uploadFiles", uploadFiles);
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}/edit")
+    public ModelAndView showEditForm(@PathVariable("id") Long id) {
+        UploadFile uploadFile = uploadFileService.findById(id);
+        UploadFileForm uploadFileForm = new UploadFileForm();
+
+        uploadFileForm.setId(uploadFile.getId());
+        uploadFileForm.setName(uploadFile.getName());
+        uploadFileForm.setDescription(uploadFile.getDescription());
+        uploadFileForm.setPublic(uploadFile.isPublic());
+        uploadFileForm.setUploadedFileUrl(uploadFile.getUploadedFile());
+
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("uploadFileForm", uploadFileForm);
+        return modelAndView;
+    }
+
+    @PostMapping("{id}/edit")
+    public ModelAndView updateFileInfo(@PathVariable("id") Long id, @ModelAttribute("uploadFile") UploadFileForm uploadFileForm) {
+        UploadFile uploadFile = uploadFileService.findById(id);
+        try {
+            if (!uploadFileForm.getUploadedFile().isEmpty()) {
+                StorageUtils.removeFeature(uploadFile.getUploadedFile());
+                String randomCode = UUID.randomUUID().toString();
+                String originFileName = uploadFileForm.getUploadedFile().getOriginalFilename();
+                String randomName = randomCode + StorageUtils.getFileExtension(originFileName);
+                uploadFileForm.getUploadedFile().transferTo(new File(StorageUtils.FEATURE_LOCATION + "/" + randomName));
+                uploadFile.setUploadedFile(randomName);
+                uploadFileForm.setUploadedFileUrl(randomName);
+            }
+            uploadFile.setId(uploadFileForm.getId());
+            uploadFile.setName(uploadFileForm.getName());
+            uploadFile.setDescription(uploadFileForm.getDescription());
+            uploadFile.setPublic(uploadFileForm.isPublic());
+            uploadFileService.save(uploadFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("uploadFileForm", uploadFileForm);
+        modelAndView.addObject("message", "File infomation is updated");
         return modelAndView;
     }
 }
